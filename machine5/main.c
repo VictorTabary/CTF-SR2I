ba3e271583db729c8fe2ecfc3f083e3fd8664c8b
0x1r1s <antoinebrn@outlook.fr>
2023-05-16 14:49:21 +0200

    update pyjail

---
+++ machine5/main.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int main() {
+    system("python3 /home/pyjail.py");
+    return 0;
+}
--- machine5/pyjail.py
+++ machine5/pyjail.py
@@ -3,22 +3,18 @@ import sys
 
 def print_first_flag():
     print("Premier flag: FLAG{55H_IS_USEFUL}")
-    
-def restricted_import(*args, **kwargs):
-    print("Accès interdit au mot 'import' !")
-
-def restricted_open(*args, **kwargs):
-    print("Accès interdit au mot 'open' !")
-
-builtins.__import__ = restricted_import
-builtins.open = restricted_open
 
 print_first_flag()
 
 while True:
     code = input(">>> ")
-    try:
-        exec(code)
-    except Exception as e:
-        print("Erreur :", e)
+    if code.__contains__("import"):
+        print("Accès interdit au mot 'import'")
+    elif code.__contains__("open"):
+        print("Accès interdit au mot 'open'")
+    else:
+        try:
+            exec(code)
+        except Exception as e:
+            print("Erreur :", e)
 
